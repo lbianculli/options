add6778131021ba8fc205da5d53d76a6cdb54986
lbianculli <35279143+lbianculli@users.noreply.github.com>
2021-03-10 08:40:13 -0500

    Update tradier_api.py

--- tradier_api.py
+++ tradier_api.py
@@ -1,11 +1,23 @@
-def get_strike_list(ticker, expiry, api_key):
-    strike_list_response = requests.get(root_url + '/options/strikes?',
-        params={'symbol': ticker, 'expiration': expiry},
-        headers={'Authorization': api_key, 'Accept': 'application/json'}
-    )
-    strikes_json = strike_list_response.json()
-    strikeList = strikes_json['strikes']['strike']
-    print("List of available strike prices: ")
-    print(strikeList)
+class TradierAPI:
+    def __init__(self, key):
+        self.key = key
+        self.root_url = "https://sandbox.tradier.com/v1/markets/
+        
+    def get_strike_list(self, ticker, exp_date):
+        strike_list_response = requests.get(self.root_url + '/options/strikes?',
+            params={'symbol': ticker, 'expiration': exp_date},
+            headers={'Authorization': self.api_key, 'Accept': 'application/json'}
+        )
+        strikes_json = strike_list_response.json()
+        strikes = strikes_json['strikes']['strike']
+        print("List of available strike prices: ")
+        print(strikes)
+
+        return strikes
     
-    return strikeList
+    def option_history(self, ticker, start_date, end_date):
+        """ get historical data for an option """
+        dates_response = requests.get(self.root_url + '/options/expirations?',
+            params={'symbol': ticker},
+            headers={'Authorization': self.key, 'Accept': 'application/json'}
+        )        
